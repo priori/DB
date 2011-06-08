26d09b3c6fc8a3028e789ecc2d268b2371a12b1c
Leonardo Priori <leonardopriori@gmail.com>
2011-06-08 18:28:42 -0300

    removing file

--- lib/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-require_once dirname(__FILE__).'/db/db.php';
-require_once dirname(__FILE__).'/db/db_result.php';
-require_once dirname(__FILE__).'/db/decode.php';
-require_once dirname(__FILE__).'/db/error.php';
-require_once dirname(__FILE__).'/db/model.php';
