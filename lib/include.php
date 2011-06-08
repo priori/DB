4e17f76c12b52975f153afd8f1bff4bd1fd02582
Leonardo Priori <leonardopriori@gmail.com>
2011-06-08 17:53:47 -0300

    include renamed

---
+++ lib/include.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once dirname(__FILE__).'/db/db.php';
+require_once dirname(__FILE__).'/db/db_result.php';
+require_once dirname(__FILE__).'/db/decode.php';
+require_once dirname(__FILE__).'/db/error.php';
+require_once dirname(__FILE__).'/db/model.php';
