e99948b1e487d9b32cd3c49f35d29471696d9c3c
priori <leonardopriori@gmail.com>
2012-07-05 23:06:18 -0300

    ...

--- DB/sql_where.php
+++ DB/sql_where.php
@@ -1,68 +1,71 @@
 <?php
-		$count = 0;
+$count = 0;
+$or = true;
+if( count( $w ){
+	$sql[] = 'WHERE ';
+}
+foreach( $w as $c => $v ){
+	if( $c === $count ){
+		if( $v === 'or' || $v === 'OR' ){
+			$q[] = " OR ";
+		}else{
+			$o = htmlspecialchars($v);
+			$this->db->fire_error("Operador desconhecido: <strong>$o</strong>");
+		}
+		$count++;
 		$or = true;
-		foreach( $w as $c => $v ){
-			if( $c === $count ){
-				if( $v === 'or' || $v === 'OR' ){
-					$q[] = " OR ";
-				}else{
-					$o = htmlspecialchars($v);
-					$this->db->fire_error("Operador desconhecido: <strong>$o</strong>");
-				}
-				$count++;
-				$or = true;
-				continue;
-			}else{
-				if( !$or ){
-					$q[] = ' AND `';
-				}else{
-					$q[] = '`';
-				}
-				$c = Decoder::decode_string($c);
-				$q[] = $this->db->escape($c[0]);
-				// verificar mais de um dentre gt, lt, ge, le, in
-				// verificar se tem alguma macro errada
-				if( isset($c['gt']) )
-					$q[] = '` > ';
-				elseif( isset($c['lt']) )
-					$q[] = '` < ';
-				elseif( isset($c['ge']) )
-					$q[] = '` >= ';
-				elseif( isset($c['le']) )
-					$q[] = '` <= ';
-				elseif( isset($c['in']) )
-					$q[] = '` in ';
-				else
-					$q[] = '` = ';
-				if( isset($c['sql']) ){
-					$q[] = '(';
-					$q[] = $v;
-					$q[] = ')';
-				}else{
-					// fazer data funcionar e verificacoes
-					if( is_array($v) && isset($c['in']) ){
-						// verificar se o array está normal
-						$b = false;
-						$q[] = '(';
-						foreach( $v as $c2 => $v2 ){
-							if( $b ){
-								$q[] = ', \'';
-							}else{
-								$q[] = '\'';
-							}
-							$q[] = $this->db->escape($v2);
-							$q[] = '\'';
-							$b = true;
-						}
-						$q[] = ')';
+		continue;
+	}else{
+		if( !$or ){
+			$q[] = ' AND `';
+		}else{
+			$q[] = '`';
+		}
+		$c = Decoder::decode_string($c);
+		$q[] = $this->db->escape($c[0]);
+		// verificar mais de um dentre gt, lt, ge, le, in
+		// verificar se tem alguma macro errada
+		if( isset($c['gt']) )
+			$q[] = '` > ';
+		elseif( isset($c['lt']) )
+			$q[] = '` < ';
+		elseif( isset($c['ge']) )
+			$q[] = '` >= ';
+		elseif( isset($c['le']) )
+			$q[] = '` <= ';
+		elseif( isset($c['in']) )
+			$q[] = '` in ';
+		else
+			$q[] = '` = ';
+		if( isset($c['sql']) ){
+			$q[] = '(';
+			$q[] = $v;
+			$q[] = ')';
+		}else{
+			// fazer data funcionar e verificacoes
+			if( is_array($v) && isset($c['in']) ){
+				// verificar se o array está normal
+				$b = false;
+				$q[] = '(';
+				foreach( $v as $c2 => $v2 ){
+					if( $b ){
+						$q[] = ', \'';
 					}else{
-						// verficar se v está normal
-						$q[] = '\'';
-						$q[] = $this->db->escape($v);
 						$q[] = '\'';
 					}
+					$q[] = $this->db->escape($v2);
+					$q[] = '\'';
+					$b = true;
 				}
-				// falta outras macros
+				$q[] = ')';
+			}else{
+				// verficar se v está normal
+				$q[] = '\'';
+				$q[] = $this->db->escape($v);
+				$q[] = '\'';
 			}
-			$or = false;
 		}
+		// falta outras macros
+	}
+	$or = false;
+}
