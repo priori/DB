f055f11a8b49b397a2f6339267a89113589d9a6a
priori <leonardopriori@gmail.com>
2012-07-12 20:12:43 -0300

    ...

--- tests/tests/date_format.php
+++ tests/tests/date_format.php
@@ -6,6 +6,5 @@ $db->pessoa->truncate();
 $db->pessoa->add(array('nome:date(dd/mm/yyyy)'=>'01/10/2011'));
 $p = $db->query('SELECT * FROM pessoa');
 $p = $p->fetch();
-echo '['.$p['nome'].']';
 test( $p['nome'] == '2011-10-01', 'Date format '.$p['nome']);
 
